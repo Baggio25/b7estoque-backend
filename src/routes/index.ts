452495e19a03e6014ff8367076b31d06292de7d8
Baggio25 <rodrigo.baggio.si@gmail.com>
2026-01-24 17:56:44 -0300

    feat: criar uma categoria

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import userRoutes from './user.routes';
 import authRoutes from './auth.routes';
+import categoryRoutes from "./category.routes";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
@@ -14,5 +15,6 @@ router.use('/auth', authRoutes);
 router.use(authMiddleware);
 
 router.use('/users', userRoutes);
+router.use("/categories", categoryRoutes);
 
 export default router;
